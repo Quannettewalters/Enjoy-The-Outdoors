9b41b79bc2597fafa57b58b42f696ab37a8a74e5
Quannette Walters <quannette1@gmail.com>
2024-11-07 14:38:09 -0500

    wrote code for sunrise and sunset/ changed nav bar for home

--- my_scripts/mountains_page.js
+++ my_scripts/mountains_page.js
@@ -3,29 +3,44 @@ document.addEventListener("DOMContentLoaded", () => {
     mountainsArray.forEach(m => inputGroupSelect.appendChild(new Option(m.name)));
     inputGroupSelect.addEventListener("change", e => {
         const selectedIndex = inputGroupSelect.selectedIndex;
-        if(selectedIndex){ 
+        if (selectedIndex) {
             const m = mountainsArray[selectedIndex - 1];
-            
-            const coords = m.coords.lat.toFixed(3) + 
-                    ", " + 
-                    m.coords.lng.toFixed(3);
+
+            const coords = m.coords.lat.toFixed(3) +
+                ", " +
+                m.coords.lng.toFixed(3);
 
             results.innerHTML = "<h1>" + m.name + "</h1>" + `                
                 Elevation:   <b>${m.elevation}</b><br>
-                Effort:      <b>${m.effort   }</b><br>
+                Effort:      <b>${m.effort}</b><br>
                 Coordinates: <b>(${coords})</b><br>
                 <br>
                 ${m.desc}
                 <br><br>
-            `; 
+            `;
             //add img image here? how?
-            if(m.img){
+            if (m.img) {
                 const i = document.createElement("img");
                 i.alt = "Mountain Image";
                 i.src = "data/images/" + m.img;
                 results.appendChild(i);
             }
         }
+        // function that can "fetch" the sunrise/sunset times
+        async function getSunsetForMountain(lat, lng) {
+            let response = await fetch(
+                `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
+            let data = await response.json();
+            return data;
+        }
+        const m = mountainsArray[inputGroupSelect.selectedIndex - 1].coords;
+        getSunsetForMountain(m.lat, m.lng).then(data => {
+            results.innerHTML += `Sunset: ${data.results.sunset}`;
+            results.innerHTML += `<br>Sunrise: ${data.results.sunrise}`;
+        });
+
+    
+
     });//end change
 });//end load
 
